13fb8588f05c16c0b888fb631ab32c05c8ac7ce0
Mihai <mciung18@student.aau.dk>
2022-04-17 14:50:29 +0200

    Rework collisions structure, add sounds

--- Assets/Scripts/SimulationEngine/Cell.cs
+++ Assets/Scripts/SimulationEngine/Cell.cs
@@ -9,7 +9,7 @@ public enum CellType
     Antibody,
     Filler,         // one filler type despite several different models, because the system does not need to distinguish between different models of filler Cells
 }
-
+[RequireComponent(typeof(AudioSource))]
 public class Cell : MonoBehaviour
 {
     private Simulation simulation;
@@ -21,11 +21,14 @@ public class Cell : MonoBehaviour
     public Transform explosionPrefab;
     public Transform ObjectGenerated;
     public Transform Antibody;
-
+    private AudioSource audioSource;
+    public AudioClip explosionSound;
+    public AudioClip sparklesSound;
 
     protected void Start()
     {
-        //simulation = GameObject.FindGameObjectWithTag("Simulator").GetComponent<Simulation>();
+        simulation = GameObject.FindGameObjectWithTag("Simulator").GetComponent<Simulation>();
+        simulation.SetAllowCollisions(true); //for testing purposes
         // TODO: should trigger spawning animation...
     }
 
@@ -44,31 +47,48 @@ public class Cell : MonoBehaviour
         // ...
     }
 
-    void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision)
     {
-        collisionCell = collision.gameObject.GetComponent<Cell>();
-        if (collisionCell.type == CellType.Pathogen)
-        {
-            if (this.type == CellType.WhiteBloodCell)
+        if (simulation.CollisionsAllowed()) { 
+            collisionCell = collision.gameObject.GetComponent<Cell>();
+            if (collisionCell.type == CellType.Pathogen)
             {
-                //bool application = simulation.ApplyForDrop(this);
-                Instantiate(sparklesPrefab, collision.transform.position, collision.transform.rotation);
-                /*if (application == true)
+                if (this.type == CellType.WhiteBloodCell)
                 {
-                    Instantiate(Antibody, collision.transform.position, collision.transform.rotation);
-                }*/
-            }
+                    //bool application = simulation.ApplyForDrop(this);
+                    //if (application == true)
+                    //{
+                    Debug.Log("WhiteCell");
+                        SpawnElements(Antibody, sparklesPrefab, collision, sparklesSound, 0.7f, false, false);
+                    //}
+                }
 
-            else if (this.type == CellType.Antibody)
-            {
-                Destroy(this.gameObject);
-                Destroy(collision.gameObject);
-                Instantiate(explosionPrefab, collision.transform.position, collision.transform.rotation);
-                Instantiate(ObjectGenerated, collision.transform.position, collision.transform.rotation);
+                else if (this.type == CellType.Antibody)
+                {
+                    Debug.Log("Antibody");
+                    SpawnElements(ObjectGenerated, explosionPrefab, collision, explosionSound, 0.7f, true, true);
+                }
+                //else 
+                // do something on other collisions
             }
-            //else 
-            // do something on other collisions
         }
+    }
+
+    public void SpawnElements(Transform Element, Transform Effect, Collision collision, AudioClip sound, float volume, bool destroySelf, bool destroyOther)
+    {
+        if (destroySelf == true)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (destroyOther == true)
+        {
+            Destroy(collision.gameObject);
+        }
+
+        Transform newInstance = Instantiate(Element, collision.transform.position, collision.transform.rotation);
+        Transform generatedEffect = Instantiate(Effect, collision.transform.position, collision.transform.rotation);
+        newInstance.gameObject.GetComponent<AudioSource>().PlayOneShot(sound, volume);
     }
 
     public void Despawn()
