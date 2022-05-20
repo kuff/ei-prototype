9961d00db70eaacfe257b10a4b68f9b25f003cf7
Peter Leth <16867891+kuff@users.noreply.github.com>
2022-05-20 13:51:18 +0200

    Add Agent

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,18 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
+[RequireComponent(typeof(AudioSource))]
 public class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("The amount of time in milliseconds to wait after invoking the Agent")]
+    public int invokeInterval = 10;
+    [Tooltip("The AudioClips which the Agent must cycle through")]
+    public List<AudioClip> clips = new List<AudioClip>(16);
+
+    private long _lastStamp = long.MaxValue;
+    
+    private bool _successful = false;
+    private int _currentIndex;
+
+    /**
+     * Starts the affective feedback clock
+     */
+    public void WakeUpAgent()
     {
-        
+        this._currentIndex = (new Random()).Next(this.clips.Count);
+        this.ResetClock();
     }
 
-    // Update is called once per frame
-    void Update()
+    private long GetUnixMillis()
     {
+        return DateTimeOffset.Now.ToUnixTimeMilliseconds();
+    }
+    
+    private void ResetClock()
+    {
+        this._lastStamp = this.GetUnixMillis();
+    }
+
+    private AudioClip DequeueNext()
+    {
+        if (this._successful) this._currentIndex += 4;
+        else this._currentIndex++;
+        if (this._currentIndex >= this.clips.Count) this._currentIndex -= this.clips.Count;
         
+        return this.clips[this._currentIndex];
+    }
+
+    protected void Start()
+    {
+        var sim = GameObject.FindGameObjectWithTag("Simulator").GetComponent<Simulation>();
+        sim.OnCollision.AddListener(s =>
+        {
+            this.ResetClock();
+            this._successful = true;
+        });
+    }
+
+    protected void Update()
+    {
+        var nowStamp = this.GetUnixMillis();
+        if (nowStamp - this._lastStamp > this.invokeInterval)
+        {
+            this.ResetClock();
+
+            var source = this.gameObject.GetComponent<AudioSource>();
+            var nextClip = this.DequeueNext();
+            Debug.Log(nextClip.name);
+            
+            source.PlayOneShot(nextClip);
+            this._successful = false;
+        }
     }
 }
